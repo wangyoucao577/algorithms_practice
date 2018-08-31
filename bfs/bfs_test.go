af24b8edc6b8d40ca79ebf70d6f921b13529c990
wangyoucao577 <wangyoucao577@gmail.com>
2018-08-31 17:30:31 +0800

    add test for bfs

---
+++ bfs/bfs_test.go
@@ -0,0 +1,63 @@
+package bfs
+
+import (
+	"testing"
+
+	"github.com/wangyoucao577/algorithms_practice/graph"
+	"github.com/wangyoucao577/algorithms_practice/graphsample1"
+)
+
+func TestBfsOnGraphSample1(t *testing.T) {
+	source := graph.NodeID(1)
+
+	type queryWant struct {
+		Depth        int
+		ShortestPath graph.Path
+	}
+	var tests = []struct {
+		target graph.NodeID
+		want   queryWant
+	}{
+		{source, queryWant{0, graph.Path{source}}},
+		{graph.NodeID(0), queryWant{1, graph.Path{source, graph.NodeID(0)}}},
+		{graph.NodeID(4), queryWant{2, graph.Path{source, graph.NodeID(0), graph.NodeID(4)}}},
+		{graph.NodeID(5), queryWant{1, graph.Path{source, graph.NodeID(5)}}},
+		{graph.NodeID(6), queryWant{2, graph.Path{source, graph.NodeID(5), graph.NodeID(6)}}},
+		{graph.NodeID(2), queryWant{2, graph.Path{source, graph.NodeID(5), graph.NodeID(2)}}},
+		{graph.NodeID(3), queryWant{3, graph.Path{source, graph.NodeID(5), graph.NodeID(2), graph.NodeID(3)}}},
+		{graph.NodeID(7), queryWant{3, graph.Path{source, graph.NodeID(5), graph.NodeID(6), graph.NodeID(7)}}},
+	}
+
+	// adjacency list based graph
+	bList, err := NewBfs(graphsample1.AdjacencyListGraphSample1, source, nil)
+	if err != nil {
+		t.Errorf("BFS on adjacency list based graph failed, source %v", source)
+	}
+	for _, v := range tests {
+		depth, path := bList.Query(v.target)
+		if depth != v.want.Depth {
+			t.Errorf("Query for %v, got depth %d, want %d", v.target, depth, v.want.Depth)
+		}
+
+		if !path.Equal(v.want.ShortestPath) {
+			t.Errorf("Query for %v, got shortest path %v, want %v", v.target, path, v.want.ShortestPath)
+		}
+	}
+
+	// adjacency matrix based graph
+	bMatrix, err := NewBfs(graphsample1.AdjacencyMatrixGraphSample1, source, nil)
+	if err != nil {
+		t.Errorf("BFS on adjacency Matrix based graph failed, source %v", source)
+	}
+	for _, v := range tests {
+		depth, path := bMatrix.Query(v.target)
+		if depth != v.want.Depth {
+			t.Errorf("Query for %v, got depth %d, want %d", v.target, depth, v.want.Depth)
+		}
+
+		if !path.Equal(v.want.ShortestPath) {
+			t.Errorf("Query for %v, got shortest path %v, want %v", v.target, path, v.want.ShortestPath)
+		}
+	}
+
+}
--- graph/graph_interface.go
+++ graph/graph_interface.go
@@ -116,3 +116,22 @@ func (g AdjacencyMatrixGraph) IterateAdjacencyNodes(currNode NodeID, action Iter
 }
 
 /************************* Adjacency Matrix Based Graph Representation *****************************/
+
+//Equal to compare whether current Path equal to another one
+func (p Path) Equal(q Path) bool {
+	if len(p) != len(q) {
+		return false
+	}
+
+	if (p == nil) != (q == nil) {
+		return false
+	}
+
+	for i := range p {
+		if p[i] != q[i] {
+			return false
+		}
+	}
+
+	return true
+}
