88bef21914f0666145b6548f2460f9f49b9a2dad
wangyoucao577 <wangyoucao577@gmail.com>
2018-09-04 22:40:31 +0800

    validdate edges attr by ch22.3 exercise 22.3-5 rules

--- dfs/dfs_test.go
+++ dfs/dfs_test.go
@@ -18,6 +18,43 @@ func (d *Dfs) printEdges(g graph.Graph) {
 	})
 	fmt.Println()
 }
+
+func (d *Dfs) ValidateEdges(g graph.Graph, t *testing.T) {
+
+	g.IterateAllNodes(func(k graph.NodeID) {
+		g.IterateAdjacencyNodes(k, func(v graph.NodeID) {
+			edge := graph.EdgeID{From: k, To: v}
+			kD := d.nodesAttr[k].timestampD
+			kF := d.nodesAttr[k].timestampF
+			vD := d.nodesAttr[v].timestampD
+			vF := d.nodesAttr[v].timestampF
+
+			switch d.edgesAttr[edge].t {
+			case branch:
+				// expect `k.D < v.D < v.F < k.F`
+				if !((kD < vD) && (vD < vF) && (vF < kF)) {
+					t.Errorf("edge %v type %v, expect %d < %d < %d < %d", edge, branch, kD, vD, vF, kF)
+				}
+			case forward:
+				// expect `k.D < v.D < v.F < k.F`
+				if !((kD < vD) && (vD < vF) && (vF < kF)) {
+					t.Errorf("edge %v type %v, expect %d < %d < %d < %d", edge, forward, kD, vD, vF, kF)
+				}
+			case backward:
+				// expect `v.D <= k.D < k.F <= v.F`
+				if !((vD <= kD) && (kD < kF) && (kF <= vF)) {
+					t.Errorf("edge %v type %v, expect %d <= %d < %d <= %d", edge, backward, vD, kD, kF, vF)
+				}
+			case cross:
+				// expect `v.D < v.F < k.D < k.F`
+				if !((vD < vF) && (vF < kD) && (kD < kF)) {
+					t.Errorf("edge %v type %v, expect %d <= %d < %d <= %d", edge, cross, vD, vF, kD, kF)
+				}
+			}
+		})
+	})
+
+}
 func TestDfsOnGraphSample2(t *testing.T) {
 
 	var tests = []struct {
@@ -39,6 +76,7 @@ func TestDfsOnGraphSample2(t *testing.T) {
 		if !reflect.DeepEqual(dList.forest, v.want) {
 			t.Errorf("DFS on graphsample2.AdjacencyListGraphSample, got forest %v, want %v", dList.forest, v.want)
 		}
+		dList.ValidateEdges(graphsample2.AdjacencyListGraphSample(), t)
 		//dList.printEdges(graphsample2.AdjacencyListGraphSample())
 		if latestDfs != nil {
 			if !reflect.DeepEqual(latestDfs, dList) {
@@ -56,6 +94,7 @@ func TestDfsOnGraphSample2(t *testing.T) {
 		if !reflect.DeepEqual(dMatrix.forest, v.want) {
 			t.Errorf("DFS on graphsample2.AdjacencyMatrixGraphSample, got forest %v, want %v", dList.forest, v.want)
 		}
+		dMatrix.ValidateEdges(graphsample2.AdjacencyMatrixGraphSample(), t)
 		//dMatrix.printEdges(graphsample2.AdjacencyMatrixGraphSample())
 		if latestDfs != nil {
 			if !reflect.DeepEqual(latestDfs, dMatrix) {
