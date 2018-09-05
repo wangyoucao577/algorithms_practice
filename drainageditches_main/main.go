7264765308683fe8af8fabe6c6abdbec2f030695
wangyoucao577 <wangyoucao577@gmail.com>
2018-09-05 10:47:10 +0800

    main for drainageditches, try to construct network flow graph first

---
+++ drainageditches_main/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/wangyoucao577/algorithms_practice/networkflowgraph"
+)
+
+func main() {
+
+	var nodeCount, edgeCount int
+	fmt.Fscanf(os.Stdin, "%d%d", &edgeCount, &nodeCount)
+	if nodeCount < 2 || edgeCount < 0 {
+		fmt.Printf("Invalid node count %d edge count %d\n", nodeCount, edgeCount)
+		return
+	}
+	g, err := networkflowgraph.ConstructNetworkFlowGraph(nodeCount, edgeCount, os.Stdin)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(g)
+}
