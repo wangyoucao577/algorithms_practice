9c405b3b636e4e55cf4dcff4e991e5507923fd96
wangyoucao577 <wangyoucao577@gmail.com>
2018-09-11 22:23:00 +0800

    graphsample4 comes from introduction to algorithms 22.5 strongly connected component

---
+++ graphsample4/sample4.go
@@ -0,0 +1,98 @@
+// Package graphsample4 defined a directed graph comes from
+//  "Introduction to Algorithms - Third Edition" 22.5
+package graphsample4
+
+import "github.com/wangyoucao577/algorithms_practice/graph"
+
+/* This sample directed graph comes from
+  "Introduction to Algorithms - Third Edition" 22.5 strongly connected component
+
+	V = 8 (node count)
+	E = 13 (edge count)
+	define directed graph G(V,E) as below:
+
+	a(0) -> b(1)  →  c(2) ← → d(3)
+	  ↑  ↙   ↓        ↓        ↓
+	e(4) →  f(5) ← → g(6)  →  h(7) ->
+								↑    ↓
+								  <-
+
+    NOTE: node `h` has a spin edge pointer to itself.
+*/
+
+const (
+	nodeCount = 8
+)
+
+func initializeGraphEdges(g graph.Graph) graph.Graph {
+
+	g.AddEdge(0, 1)
+
+	g.AddEdge(1, 2)
+	g.AddEdge(1, 4)
+	g.AddEdge(1, 5)
+
+	g.AddEdge(2, 3)
+	g.AddEdge(2, 6)
+
+	g.AddEdge(3, 2)
+	g.AddEdge(3, 7)
+
+	g.AddEdge(4, 0)
+	g.AddEdge(4, 5)
+
+	g.AddEdge(5, 6)
+
+	g.AddEdge(6, 5)
+	g.AddEdge(6, 7)
+
+	g.AddEdge(7, 7)
+	return g
+}
+
+type nodeIDNameConverter struct {
+	orderedNodesName []string
+	nodeNameToIDMap  map[string]graph.NodeID
+}
+
+// define fixed nodes order in the graph, then we use the `index` as nodeID for search,
+// will be easier to implement by code.
+// node name only for print.
+var nodeConverter = nodeIDNameConverter{
+	[]string{"a", "b", "c", "d", "e", "f", "g", "h"},
+	map[string]graph.NodeID{}, // will be inited during import
+}
+
+// initialization during package import
+func init() {
+	for i, v := range nodeConverter.orderedNodesName {
+		nodeConverter.nodeNameToIDMap[v] = graph.NodeID(i)
+	}
+}
+
+// IDToName convert NodeID to human readable name
+func IDToName(i graph.NodeID) string {
+	if i == graph.InvalidNodeID {
+		return "InvalidNodeID"
+	}
+	return nodeConverter.orderedNodesName[i]
+}
+
+// NameToID convert node human readable name to NodeID
+func NameToID(name string) graph.NodeID {
+	return nodeConverter.nodeNameToIDMap[name]
+}
+
+// AdjacencyListGraphSample return the adjacency list based graph sample instance
+func AdjacencyListGraphSample() graph.Graph {
+	sample := graph.NewAdjacencyListGraph(nodeCount)
+
+	return initializeGraphEdges(sample)
+}
+
+// AdjacencyMatrixGraphSample return the adjacency matrix based graph sample instance
+func AdjacencyMatrixGraphSample() graph.Graph {
+	sample := graph.NewAdjacencyMatrixGraph(nodeCount)
+
+	return initializeGraphEdges(sample)
+}
---
+++ graphsample4/sample4_test.go
@@ -0,0 +1,61 @@
+package graphsample4
+
+import (
+	"testing"
+
+	"github.com/wangyoucao577/algorithms_practice/graph"
+)
+
+func TestAdjacencyListGraphSample(t *testing.T) {
+
+	want := [][]graph.NodeID{
+		[]graph.NodeID{1},
+		[]graph.NodeID{2, 4, 5},
+		[]graph.NodeID{3, 6},
+		[]graph.NodeID{2, 7},
+		[]graph.NodeID{0, 5},
+		[]graph.NodeID{6},
+		[]graph.NodeID{5, 7},
+		[]graph.NodeID{7},
+	}
+
+	g := AdjacencyListGraphSample()
+
+	if g.NodeCount() != nodeCount {
+		t.Errorf("new adjacency list graph sample, got node count %d, want %d", g.NodeCount(), nodeCount)
+	}
+
+	wantEdgeCount := 0
+	g.IterateAllNodes(func(u graph.NodeID) {
+		i := graph.NodeID(0)
+		adjNodesCount := 0
+		wantEdgeCount += len(want[u])
+
+		g.IterateAdjacencyNodes(u, func(v graph.NodeID) {
+			adjNodesCount++
+			if adjNodesCount > len(want[u]) {
+				t.Errorf("adjacency list graph sample, node %v, got %d adjacency nodes, but want %d adjacency nodes",
+					u, adjNodesCount, len(want[u]))
+			} else {
+				if v != want[u][i] {
+					t.Errorf("adjacency list graph sample, node %v, %dth adjacency node, %v, but want %v",
+						u, i, v, want[u][i])
+				}
+				i++
+			}
+		})
+	})
+
+	if g.EdgeCount() != wantEdgeCount {
+		t.Errorf("new adjacency list graph sample, got edge count %d, want %d", g.EdgeCount(), wantEdgeCount)
+	}
+
+	//id <-> name map verify
+	for i, v := range nodeConverter.orderedNodesName {
+		gotNodeID, ok := nodeConverter.nodeNameToIDMap[v]
+		if !ok || graph.NodeID(i) != gotNodeID {
+			t.Errorf("NodeID of name %v not match, expect NodeID %v but got %v (exist in map %v)",
+				v, graph.NodeID(i), gotNodeID, ok)
+		}
+	}
+}
