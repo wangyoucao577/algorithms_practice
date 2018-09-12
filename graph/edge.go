b6ba2f741ba4790d43a949ddeb3b89ba01b88dad
wangyoucao577 <wangyoucao577@gmail.com>
2018-09-12 16:14:24 +0800

    delete a useless method

--- graph/edge.go
+++ graph/edge.go
@@ -22,9 +22,3 @@ func (e EdgeID) UndirectedEqual(f EdgeID) bool {
 func (e EdgeID) Reverse() EdgeID {
 	return EdgeID{e.To, e.From}
 }
-
-//IsValid return whether the edgeID is valid
-//if From or To is InvalidNodeID, the edgeID is invalid
-func (e EdgeID) IsValid() bool {
-	return e.From != InvalidNodeID && e.To != InvalidNodeID
-}
