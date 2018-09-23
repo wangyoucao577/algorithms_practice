14f44c26db736ea1f7088f06e8f0fc77cf7e70c9
wangyoucao577 <wangyoucao577@gmail.com>
2018-09-23 22:55:13 +0800

    implment insertion-sort

--- mysorts/insertion_sort.go
+++ mysorts/insertion_sort.go
@@ -2,8 +2,26 @@ package mysorts
 
 // insertionSort implements Insertion Sort
 func insertionSort(in []int) []int {
+	if len(in) <= 1 {
+		return in
+	}
 
-	//TODO: implement ...
+	// sort in-place
+	for i := 1; i < len(in); i++ {
+		curr := in[i]
+
+		// insert in[i] into sorted in[0] ~ in[i-1]
+		// compare from i-1 to 0
+		j := i - 1
+		for ; j >= 0; j-- {
+			if curr < in[j] { //Less()
+				in[j+1] = in[j] //Swap()
+			} else {
+				break
+			}
+		}
+		in[j+1] = curr //Swap()
+	}
 
 	return in
 }
