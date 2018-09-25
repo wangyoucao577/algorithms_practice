fb0db8eb94dbd2cd31d7750f6b74e2d460b700db
wangyoucao577 <wangyoucao577@gmail.com>
2018-09-25 21:33:47 +0800

    add merge-sort aux array based implementation

--- mysorts/merge_sort.go
+++ mysorts/merge_sort.go
@@ -1,11 +1,19 @@
 package mysorts
 
-// insertionSort implements Merge Sort In-place
+import (
+	"math"
+)
+
+/************************* In-place implementation *************************/
+
+// mergeSort implements Merge Sort In-place
 func mergeSort(in myInterface) {
 
 	mergeSortImpl(in, 0, in.Len()-1)
 }
 
+// mergeSortImpl split the array to two parts, then recurses call itself for them.
+//  after that merge results of the two parts.
 func mergeSortImpl(in myInterface, p, r int) {
 
 	if p < r {
@@ -36,3 +44,57 @@ func merge(in myInterface, p, q, r int) {
 
 	}
 }
+
+/************************* In-place implementation *************************/
+
+/************************* With aux array implementation *************************/
+
+// mergeSortAuxArrayBased implements Merge Sort with aux array
+func mergeSortAuxArrayBased(in []int) {
+	mergeSortImplAuxArrayBased(in, 0, len(in)-1)
+}
+
+// mergeSortImplAuxArrayBased split the array to two parts, then recurses call itself for them.
+//  after that merge results of the two parts.
+// Same process flow as mergeSortImpl
+func mergeSortImplAuxArrayBased(in []int, p, r int) {
+
+	if p < r {
+		q := (p + r) / 2
+		mergeSortImplAuxArrayBased(in, p, q)
+		mergeSortImplAuxArrayBased(in, q+1, r)
+		mergeAuxArrayBased(in, p, q, r)
+	}
+}
+
+func mergeAuxArrayBased(in []int, p, q, r int) {
+	n1 := q - p + 1
+	n2 := r - q // r - (q+1) + 1
+
+	// create aux arrays
+	auxArray1 := make([]int, n1+1, n1+1)
+	auxArray2 := make([]int, n2+1, n2+1)
+	for i := 0; i < n1; i++ {
+		auxArray1[i] = in[p+i]
+	}
+	auxArray1[n1] = math.MaxInt32 // set last value to infinity, only for aux
+	for i := 0; i < n2; i++ {
+		auxArray2[i] = in[q+1+i]
+	}
+	auxArray2[n2] = math.MaxInt32 // set last value to infinity, only for aux
+
+	// merge two aux arrays into original array
+	i, j := 0, 0
+	for k := p; k <= r; k++ {
+		if auxArray1[i] <= auxArray2[j] {
+			in[k] = auxArray1[i]
+			i++
+		} else {
+			in[k] = auxArray2[j]
+			j++
+		}
+	}
+
+}
+
+/************************* With aux array implementation *************************/
--- mysorts/sorts_test.go
+++ mysorts/sorts_test.go
@@ -63,12 +63,22 @@ func TestSortFunctionalWithRandomCases(t *testing.T) {
 			break
 		}
 
-		//merge sort
+		//merge sort in-place implementation
 		caseForMergeSort := testCase.deepCopy()
 		mergeSort(caseForMergeSort)
 		if !isSorted(caseForMergeSort, true) {
-			t.Errorf("mergeSort failed on \n%v\n", caseForMergeSort)
+			t.Errorf("in-place mergeSort failed on \n%v\n", caseForMergeSort)
 			break
 		}
+
+		//merge sort aux array based implementation
+		// NOTE: can not done by only sort.Interface
+		caseForMergeSortAuxArrayBased := testCase.deepCopy()
+		mergeSortAuxArrayBased(caseForMergeSortAuxArrayBased)
+		if !isSorted(caseForMergeSortAuxArrayBased, true) {
+			t.Errorf("aux array based mergeSort failed on \n%v\n", caseForMergeSortAuxArrayBased)
+			break
+		}
+
 	}
 }
