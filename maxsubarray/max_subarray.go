95bdc2971db094d55fe3dfa036a841c1e12253c3
wangyoucao577 <wangyoucao577@gmail.com>
2018-09-26 14:26:05 +0800

    implement max subarray by divide and conquer method

--- maxsubarray/max_subarray.go
+++ maxsubarray/max_subarray.go
@@ -5,9 +5,60 @@ package maxsubarray
 // return the the subarray
 func findMaxSubarray(in []int) []int {
 
-	out := []int{}
-
-	//TODO:
-
+	l, r, _ := findMaxSubarrayImpl(in, 0, len(in)-1)
+	out := in[l : r+1]
 	return out
+}
+
+// findMaxSubarrayImpl return left-index, right-index and sum value of the subarray
+func findMaxSubarrayImpl(in []int, low, high int) (int, int, int) {
+
+	if low == high {
+		return low, high, in[low]
+	}
+
+	mid := (low + high) / 2
+	leftLow, leftHigh, leftSum := findMaxSubarrayImpl(in, low, mid)
+	rightLow, rightHigh, rightSum := findMaxSubarrayImpl(in, mid+1, high)
+	crossingLow, crossingHigh, crossingSum := findMaxCrossingSubarray(in, low, mid, high)
+
+	if leftSum >= rightSum && leftSum >= crossingSum {
+		return leftLow, leftHigh, leftSum
+	}
+	//else if
+	if rightSum >= leftSum && rightSum >= crossingSum {
+		return rightLow, rightHigh, rightSum
+	}
+	//else
+	return crossingLow, crossingHigh, crossingSum
+}
+
+// findMaxCrossingSubarray return left-index, right-index and sum value of the crossing subarray
+func findMaxCrossingSubarray(in []int, low, mid, high int) (int, int, int) {
+
+	leftLow := mid
+	leftSum := in[mid]
+
+	sum := leftSum
+	for i := mid - 1; i >= low; i-- {
+		sum += in[i]
+		if sum > leftSum {
+			leftLow = i
+			leftSum = sum
+		}
+	}
+
+	rightHigh := mid + 1
+	rightSum := in[mid+1]
+
+	sum = rightSum
+	for i := mid + 2; i <= high; i++ {
+		sum += in[i]
+		if sum > rightSum {
+			rightHigh = i
+			rightSum = sum
+		}
+	}
+
+	return leftLow, rightHigh, leftSum + rightSum
 }
