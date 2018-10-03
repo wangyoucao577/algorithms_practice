580b136a894765e5bc1fbe546eccda4f807f7220
wangyoucao577 <wangyoucao577@gmail.com>
2018-10-03 11:09:06 +0800

    loop based maxHeapity instead of rescurse based

--- mysorts/heap_sort.go
+++ mysorts/heap_sort.go
@@ -7,12 +7,18 @@ func heapSort(in myInterface) {
 	}
 
 	bh := binaryHeap{}
-	bh.buildMaxHeap(in)
+	bh.maxHeapity = bh.maxHeapityLoopBased //switch loop or rescurse based maxheapity here
+
+	bh.buildMaxHeap(in) // first build max heap
 
 	for i := in.Len(); i >= 2; i-- { // 1 started index here
 		in.Swap(0, i-1) // move biggest element to the end
 		bh.heapSize--
-		bh.maxHeapity(in, 1) // then find the biggest and move it to root again
+
+		// then find the biggest and move it to root again
+		//bh.maxHeapityRecursed(in, 1)
+		//bh.maxHeapityLoopBased(in, 1)
+		bh.maxHeapity(in, 1)
 	}
 }
 
@@ -20,7 +26,11 @@ func heapSort(in myInterface) {
 // NOTE: an outside slice will be operated based on these methods.
 type binaryHeap struct {
 	heapSize int
+
+	maxHeapity func(myInterface, int) //maxHeapity func
 }
+
+/********************** Be aware that `i` in below functions are all started by 1, not 0 *****************/
 
 func (m binaryHeap) leftChild(i int) int {
 	return i * 2
@@ -32,9 +42,9 @@ func (m binaryHeap) parent(i int) int {
 	return i / 2
 }
 
-// maxHeapity to keep max heap properties
+// maxHeapityRecurseBased to keep max heap properties recursed
 // i is node index started by 1, only will be translated to 0 started index until operate array
-func (m binaryHeap) maxHeapity(in myInterface, i int) {
+func (m *binaryHeap) maxHeapityRecurseBased(in myInterface, i int) {
 	// i will be the parent in the maxHeapity procedure
 	left := m.leftChild(i)
 	right := m.rightChild(i)
@@ -51,7 +61,36 @@ func (m binaryHeap) maxHeapity(in myInterface, i int) {
 
 	if largest != i {
 		in.Swap(largest-1, i-1) // translate to 0 started index to operate array
-		m.maxHeapity(in, largest)
+		m.maxHeapityRecurseBased(in, largest)
+	}
+}
+
+// maxHeapityLoopBased to keep max heap properties by loop
+// i is node index started by 1, only will be translated to 0 started index until operate array
+func (m *binaryHeap) maxHeapityLoopBased(in myInterface, i int) {
+
+	for {
+
+		// i will be the parent in the maxHeapity procedure
+		left := m.leftChild(i)
+		right := m.rightChild(i)
+
+		largest := i
+
+		// find largest from i,left,right
+		if left <= m.heapSize && in.Less(largest-1, left-1) { // translate to 0 started index to operate array
+			largest = left
+		}
+		if right <= m.heapSize && in.Less(largest-1, right-1) { // translate to 0 started index to operate array
+			largest = right
+		}
+
+		if largest == i {
+			break
+		}
+
+		in.Swap(largest-1, i-1) // translate to 0 started index to operate array
+		i = largest
 	}
 }
 
@@ -60,6 +99,8 @@ func (m *binaryHeap) buildMaxHeap(in myInterface) {
 
 	// start at in.Len()/2 because all leaf nodes don't need to do the `maxheapity`
 	for i := in.Len() / 2; i >= 1; i-- { // 1 started index here
+		//m.maxHeapityRecursed(in, i)
+		//m.maxHeapityLoopBased(in, i)
 		m.maxHeapity(in, i)
 	}
 }
