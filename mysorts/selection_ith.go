90efde83899931bffdcb6c9cf9293743cf6e5e1c
wangyoucao577 <wangyoucao577@gmail.com>
2018-10-06 16:27:53 +0800

    selection i-th implemented by loop

--- mysorts/selection_ith.go
+++ mysorts/selection_ith.go
@@ -8,7 +8,8 @@ func Select(in myInterface, i int) int {
 		return -1
 	}
 
-	return selectImplByRescurse(in, i, 0, in.Len()-1)
+	//return selectImplByRescurse(in, i, 0, in.Len()-1)
+	return selectImplByLoop(in, i, 0, in.Len()-1)
 }
 
 func selectImplByRescurse(in myInterface, i, p, r int) int {
@@ -21,4 +22,20 @@ func selectImplByRescurse(in myInterface, i, p, r int) int {
 		return selectImplByRescurse(in, i, p, q-1)
 	}
 	return selectImplByRescurse(in, i, q+1, r)
+}
+
+func selectImplByLoop(in myInterface, i, p, r int) int {
+
+	for {
+		q := randomizedPartition(in, p, r)
+		if q == i {
+			return q
+		}
+
+		if q > i {
+			r = q - 1
+		} else {
+			p = q + 1
+		}
+	}
 }
