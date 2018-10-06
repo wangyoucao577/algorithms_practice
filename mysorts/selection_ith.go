24cffef87428f40633483ab5f1bc32171452ecfd
wangyoucao577 <wangyoucao577@gmail.com>
2018-10-06 16:23:13 +0800

    selection i-th algorithm

---
+++ mysorts/selection_ith.go
@@ -0,0 +1,24 @@
+package mysorts
+
+// Select select i-th order statistic from the input array,
+// return the index of the i-th element if succeed, -1 if failed.
+// based on randomizedPartition() of quicksort
+func Select(in myInterface, i int) int {
+	if i < 0 || i >= in.Len() {
+		return -1
+	}
+
+	return selectImplByRescurse(in, i, 0, in.Len()-1)
+}
+
+func selectImplByRescurse(in myInterface, i, p, r int) int {
+	q := randomizedPartition(in, p, r)
+	if q == i {
+		return q
+	}
+
+	if q > i {
+		return selectImplByRescurse(in, i, p, q-1)
+	}
+	return selectImplByRescurse(in, i, q+1, r)
+}
---
+++ mysorts/selection_ith_test.go
@@ -0,0 +1,69 @@
+package mysorts
+
+import "testing"
+
+func TestSelectByPredefinedCases(t *testing.T) {
+	cases := []struct {
+		array intSlice
+		i     int
+		want  int
+	}{
+		{intSlice{}, 0, -1},
+		{intSlice{}, -1, -1},
+		{intSlice{0}, 0, 0},
+		{intSlice{1}, 0, 0},
+		{intSlice{0, 1}, 1, 1},
+	}
+
+	for _, v := range cases {
+		got := Select(v.array, v.i)
+		if got != v.want {
+			t.Errorf("for %d-th of input array %v, expect %v but got %v", v.i, v.array, v.want, got)
+		}
+	}
+}
+
+func TestSelectByRandomCases(t *testing.T) {
+	randomCaseCount := 100
+	maxArrayLen := 1000
+
+	for j := 0; j < randomCaseCount; j++ {
+		//generate case
+		testCase := generateRandomCase(maxArrayLen)
+		if testCase.Len() == 0 {
+			continue
+		}
+
+		//sort for check result
+		caseForQuickSort := testCase.deepCopy()
+		QuickSort(caseForQuickSort)
+		if !isSorted(caseForQuickSort, true) {
+			t.Errorf("in-place QuickSort failed on \n%v\n", caseForQuickSort)
+			break
+		}
+
+		median := testCase.Len() / 2
+		caseForSelection := testCase.deepCopy()
+		got := Select(caseForSelection, median)
+		if caseForSelection[got] != caseForQuickSort[median] {
+			t.Errorf("Select median %v in %v, want %v but got %v", median, testCase, caseForQuickSort[median], caseForSelection[got])
+			break
+		}
+	}
+}
+
+func BenchmarkWorstCaseSelectionMedian(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		testCase := generateWorstCase(benchmarkMaxArrayLen)
+		median := testCase.Len() / 2
+		Select(testCase, median)
+	}
+}
+
+func BenchmarkBestCaseSelectionMedian(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		testCase := generateBestCase(benchmarkMaxArrayLen)
+		median := testCase.Len() / 2
+		Select(testCase, median)
+	}
+}
