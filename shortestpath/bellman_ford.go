8bfdbbb2f61cfe637d95fb23c6d1de3a90d7bdab
wangyoucao577 <wangyoucao577@gmail.com>
2018-11-05 17:33:03 +0800

    bellman-ford for single-source shortest path TODO: test

---
+++ shortestpath/bellman_ford.go
@@ -0,0 +1,33 @@
+package shortestpath
+
+import (
+	"github.com/wangyoucao577/algorithms_practice/graph"
+	"github.com/wangyoucao577/algorithms_practice/weightedgraph"
+)
+
+// BellmanFord implements bellman-ford algorithm for single-source shortest path problem
+func BellmanFord(g weightedgraph.WeightedGraph, s graph.NodeID) bool {
+
+	sp := &shortestPath{}
+	sp.initializeSingleSource(g, s)
+
+	v := g.NodeCount()
+	for i := 0; i < v-1; i++ { // iterate v-1 times
+		g.IterateEdges(func(e graph.EdgeID) {
+			w, _ := g.Weight(e.From, e.To)
+			sp.relax(e.From, e.To, w)
+		})
+	}
+
+	negativeCycle := false
+	g.IterateEdges(func(e graph.EdgeID) {
+		w, _ := g.Weight(e.From, e.To)
+		if sp.relaxable(e.From, e.To, w) {
+
+			//TODO: improvable - if negative cycle found, then can return directly.
+			negativeCycle = true
+		}
+	})
+
+	return !negativeCycle
+}
---
+++ shortestpath/single_source_shortest_path.go
@@ -0,0 +1,45 @@
+// Package shortestpath implmenets single-source shortest paths algorithms
+package shortestpath
+
+import (
+	"github.com/wangyoucao577/algorithms_practice/graph"
+	"github.com/wangyoucao577/algorithms_practice/weightedgraph"
+)
+
+const (
+	infinitelyWeight weightedgraph.Weight = weightedgraph.Weight((^uint(0)) >> 1)
+)
+
+type nodeAttr struct {
+	d      weightedgraph.Weight // weight from source to current node
+	parent graph.NodeID         // remember parent node, InvalidNodeID means no parent
+}
+
+type shortestPath struct {
+	nodesMap map[graph.NodeID]*nodeAttr
+}
+
+func (sp *shortestPath) initializeSingleSource(g weightedgraph.WeightedGraph, s graph.NodeID) {
+
+	sp.nodesMap = map[graph.NodeID]*nodeAttr{}
+	g.IterateAllNodes(func(u graph.NodeID) {
+		sp.nodesMap[u].d = infinitelyWeight
+		sp.nodesMap[u].parent = graph.InvalidNodeID
+	})
+
+	sp.nodesMap[s].d = 0
+}
+
+func (sp *shortestPath) relax(u, v graph.NodeID, w weightedgraph.Weight) {
+	if sp.nodesMap[v].d > sp.nodesMap[u].d+w { // can handle infinitelyWeight since we set it's too max
+		sp.nodesMap[v].d = sp.nodesMap[u].d + w
+		sp.nodesMap[v].parent = u
+	}
+}
+
+func (sp shortestPath) relaxable(u, v graph.NodeID, w weightedgraph.Weight) bool {
+	if sp.nodesMap[v].d > sp.nodesMap[u].d+w { // can handle infinitelyWeight since we set it's too max
+		return true
+	}
+	return false
+}
