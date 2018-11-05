93ae6366e260d2ac2ce98fefcbe231181545553a
wangyoucao577 <wangyoucao577@gmail.com>
2018-11-05 22:54:16 +0800

    dijkstra

---
+++ singlesourceshortestpaths/dijkstra.go
@@ -0,0 +1,53 @@
+package singlesourceshortestpaths
+
+import (
+	"github.com/wangyoucao577/algorithms_practice/graph"
+	"github.com/wangyoucao577/algorithms_practice/weightedgraph"
+)
+
+// Dijkstra implements the dijkstra shortest path algorithm
+func Dijkstra(g *weightedgraph.WeightedGraph, s graph.NodeID) *ShortestPaths {
+
+	sp := &ShortestPaths{}
+	sp.initializeSingleSource(g, s)
+
+	q := []graph.NodeID{}
+	g.IterateAllNodes(func(u graph.NodeID) {
+		q = append(q, u)
+	})
+
+	for len(q) > 0 {
+		u, newQ := sp.extractMin(q)
+		q = newQ
+
+		g.IterateAdjacencyNodes(u, func(v graph.NodeID) {
+			w, _ := g.Weight(u, v)
+			sp.relax(u, v, w)
+		})
+	}
+
+	return sp
+}
+
+func (sp *ShortestPaths) extractMin(q []graph.NodeID) (graph.NodeID, []graph.NodeID) {
+	i := 0
+	min := sp.nodesMap[q[i]].d
+
+	for j := 1; j < len(q); j++ {
+		if sp.nodesMap[q[j]].d < min {
+			i = j
+			min = sp.nodesMap[q[j]].d
+		}
+	}
+	u := q[i]
+
+	if i == 0 {
+		q = q[1:]
+	} else if i == len(q)-1 {
+		q = q[:i]
+	} else {
+		q = append(q[:i], q[i+1:]...)
+	}
+
+	return u, q
+}
--- singlesourceshortestpaths/shortest_paths_test.go
+++ singlesourceshortestpaths/shortest_paths_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/wangyoucao577/algorithms_practice/graph"
 	"github.com/wangyoucao577/algorithms_practice/graphsample6"
+	"github.com/wangyoucao577/algorithms_practice/graphsample8"
 	"github.com/wangyoucao577/algorithms_practice/weightedgraph"
 )
 
@@ -39,4 +40,33 @@ func TestSingleSourceShortestPathSearch(t *testing.T) {
 			t.Errorf("want shortest path %v, but got %v", v.path, path)
 		}
 	}
+}
+
+func TestDijkstra(t *testing.T) {
+	sp := Dijkstra(graphsample8.GraphSample(), graphsample8.NameToID("s"))
+
+	wantCosts := []weightedgraph.Weight{0, 8, 9, 5, 7}
+	for k, v := range sp.nodesMap {
+		if v.d != wantCosts[k] {
+			t.Errorf("Dijkstra calulated, expect cost %v from %v to %v, but got %v",
+				v.d, sp.s, k, wantCosts[k])
+		}
+	}
+
+	wantShortestPaths := []struct {
+		destination graph.NodeID
+		path        graph.Path
+	}{
+		{3, graph.Path{0, 3}},
+		{1, graph.Path{0, 3, 1}},
+		{4, graph.Path{0, 3, 4}},
+	}
+
+	for _, v := range wantShortestPaths {
+		path, _ := sp.RetrievePath(v.destination)
+		if !reflect.DeepEqual(path, v.path) {
+			t.Errorf("want shortest path %v, but got %v", v.path, path)
+		}
+	}
+
 }
