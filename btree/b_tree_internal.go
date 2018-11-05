b0d1ee8fbd870d3ae098f09311429b1b0ebf948f
wangyoucao577 <wangyoucao577@gmail.com>
2018-11-05 11:28:05 +0800

    more validate for b-tree node

--- btree/b_tree_internal.go
+++ btree/b_tree_internal.go
@@ -18,6 +18,10 @@ func (b BTree) validateNode(x *treeNode) error {
 		return nil
 	}
 
+	if x.n != len(x.keys) {
+		return fmt.Errorf("expect x.n == len(x.keys), but got %v != %v", x.n, len(x.keys))
+	}
+
 	if x != b.root {
 		if x.n <= b.t-1 || x.n >= 2*b.t-1 {
 			return fmt.Errorf("treeNode %v keys count %v out of range [%v,%v]", x, x.n, b.t-1, 2*b.t-1)
@@ -27,6 +31,21 @@ func (b BTree) validateNode(x *treeNode) error {
 	for i := 1; i < x.n; i++ {
 		if x.keys[i-1] > x.keys[i] {
 			return fmt.Errorf("expect keys[%v] <= keys[%v], but got %v > %v", i-1, i, x.keys[i-1], x.keys[i])
+		}
+	}
+
+	if !x.isLeaf {
+		if x.n+1 != len(x.childs) {
+			return fmt.Errorf("expect x.n+1 == len(x.childs), but got %v != %v", x.n+1, len(x.childs))
+		}
+
+		for i := 0; i < x.n; i++ {
+			k1 := x.childs[i].keys[x.childs[i].n-1]
+			k2 := x.childs[i+1].keys[0]
+
+			if k1 > x.keys[i] || x.keys[i] > k2 {
+				return fmt.Errorf("expect k1 <= x.key[%v] <= k2, but got %v %v %v", i, k1, x.keys[i], k2)
+			}
 		}
 	}
 
